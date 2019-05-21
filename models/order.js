27836776ebf4c41e07f728ae4732c00e5d532307
Christian-Jared Cabanero <cjcabanero@Christian-Jareds-MacBook-Air.local>
2019-05-21 15:49:30 -0700

    Updated App

--- models/order.js
+++ models/order.js
@@ -4,9 +4,9 @@ var Schema = mongoose.Schema;
 var postSchema = new Schema({
   title: { type: String, required: true },
   address: { type: String, required: true },
-  type: { type: String, enum: ['donut-holes', 'glazed', 'chocolate', 'strawberry', 'filled donut', 'coffee'], required: true },
+  type: { type: String, enum: ['donut holes', 'glazed', 'chocolate', 'strawberry', 'jelly filled', 'chocolate filled', 'custard filled', 'bacon egg ham cheese croissant', 'sausage ccmuffin w/ egg', 'ccs filipino special', 'ccs creamy warm milk', 'coffee', 'soft drinks'], required: true },
   request: { type: String, required: true },
-  category: { type: String, enum: ['donuts', 'breakfast-sandwiches', 'drinks'], required: true },
+  category: { type: String, enum: ['donuts', 'breakfast sandwiches', 'ccs specials', 'drinks'], required: true },
   upvotes: {type: Number, default: 0},
   downvotes: {type: Number, default: 0},
   date: { type: Date, default: Date.now }
